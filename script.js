3e27195f661468d5fc91c1218caf4c62467a6330
heruni <herunisp@gmail.com>
2026-06-16 22:42:55 +0530

    initialize 16x16 grid container using flexbox and js loop

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-!   
+const container = document.querySelector('.container');
+const clearBtn = document.querySelector('#clear');
+
+function createGrid(size) {
+    for (let i = 0; i < 16 * 16; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        container.appendChild(div);
+    }
+}
+
+createGrid(16);
